32d722da480bfaa694cade7c68d5b57973d51271
dirky7-dev <danieldirky7@gmail.com>
2026-01-11 04:53:52 +0100

    added components

---
+++ src/app/calendar-type/calendar-type.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-type',
+  imports: [],
+  templateUrl: './calendar-type.html',
+  styleUrl: './calendar-type.css',
+})
+export class CalendarType {
+
+}
